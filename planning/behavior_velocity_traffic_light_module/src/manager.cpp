paulsohn/autoware_universe_planning_2

feat(rtc_interface)!: add new field to rtc cooperate status (#6933)
* feat(rtc_interface): add new field

Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

* feat(bvp): support new rtc cooperate status

Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

* feat(bpp): support new rtc cooperate status

Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

---------

Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

planning/behavior_velocity_traffic_light_module/src/manager.cpp
@@ -133,7 +133,8 @@ void TrafficLightModuleManager::launchNewModules(
         planner_param_, logger_.get_child("traffic_light_module"), clock_));
       generateUUID(module_id);
       updateRTCStatus(
-        getUUID(module_id), true, std::numeric_limits<double>::lowest(), path.header.stamp);
+        getUUID(module_id), true, State::WAITING_FOR_EXECUTION,
+        std::numeric_limits<double>::lowest(), path.header.stamp);
     }
   }
 }

sai-hideyasu/autoware.universe

planning/behavior_velocity_traffic_light_module/src/manager.cpp
@@ -132,7 +132,8 @@ void TrafficLightModuleManager::launchNewModules(
         logger_.get_child("traffic_light_module"), clock_));
       generateUUID(lane_id);
       updateRTCStatus(
-        getUUID(lane_id), true, std::numeric_limits<double>::lowest(), path.header.stamp);
+        getUUID(lane_id), true, State::WAITING_FOR_EXECUTION,
+        std::numeric_limits<double>::lowest(), path.header.stamp);
     }
   }
 }